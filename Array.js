c5bb333c0502b545e861453f05329b37cab32280
Dewayne Petty II <christsiblings@gmail.com>
2020-07-31 03:00:10 -0500

    array commit

---
+++ Array.js
@@ -0,0 +1,23 @@
+//Exercise 1
+let names = ['Greg', 'Mary', 'Devon', 'James',]
+names.forEach(element => console.log(element))
+
+// Exercise 2
+names.shift ('Greg')
+console.log ('Exercise 2')
+names.forEach(element => console.log(element))
+
+//Exercise 3
+names.pop()
+console.log('Exercise 3')
+names.forEach(element => console.log(element))
+
+//Exercise 4
+names.unshift('Matt')
+console.log('Exercise 4')
+names.forEach(element => console.log(element))
+
+//Exercise 5
+names.push('Dewayne')
+console.log('Exercise 5')
+names.forEach(element => console.log(element))
